19b1d42770e195b0d6c9df7f07010f44c52bd41d
T-OJO <113072540+T-OJO@users.noreply.github.com>
2022-11-02 17:13:29 -0400

    Update sketch.js

--- userObject/sketch.js
+++ userObject/sketch.js
@@ -1,7 +1,11 @@
 function setup() {
   createCanvas(400, 400);
+  colorMode(RGB);
+  background(100,200,50);
 }
 
 function draw() {
   background(220);
+  rect(100, 100, 20,20);
+  console.log('test');
 }
